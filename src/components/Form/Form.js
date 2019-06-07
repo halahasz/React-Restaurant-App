86c83e862520b280e3b46691d99ff8723dbf99fa
Halina Balys <hbalys@gmail.com>
2019-06-07 18:49:14 +0200

    Created FormRadio component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,6 +3,7 @@ import styles from "./Form.module.scss";
 import Input from "../Input/Input";
 import Button from "../Button/Button";
 import Title from "../Title/Title";
+import FormRadio from "./FormRadio";
 
 const types = {
   menu: "menu",
@@ -11,10 +12,10 @@ const types = {
 };
 
 const descriptions = {
-  menu: 'menu item',
-  news: 'article',
-  gallery: 'image'
-}
+  menu: "menu item",
+  news: "article",
+  gallery: "image"
+};
 
 class Form extends React.Component {
   state = {
@@ -36,33 +37,29 @@ class Form extends React.Component {
           className={styles.form}
           onSubmit={this.props.submitFn}
         >
-        <label className={styles.radio} htmlFor={types.menu}>
-          <input
-            id={types.menu}
-            type="radio"
-            checked={this.state.activeOption === types.menu}
-            onChange={() => this.handleRadioButtonCHange(types.menu)}
-          />
-          <div className={styles.radioButton} />
-          Menu</label>
-          <label className={styles.radio}  htmlFor={types.news}>
-          <input
-            id={types.news}
-            type="radio"
-            checked={this.state.activeOption === types.news}
-            onChange={() => this.handleRadioButtonCHange(types.news)}
-          />
-          <div className={styles.radioButton} />
-          News</label>
-          <label className={styles.radio} htmlFor={types.gallery}>
-          <input
-            id={types.gallery}
-            type="radio"
-            checked={this.state.activeOption === types.gallery}
-            onChange={() => this.handleRadioButtonCHange(types.gallery)}
-          />
-          <div className={styles.radioButton} />
-          Gallery</label>
+          <div className={styles.radioWrapper}>
+            <FormRadio
+              id={types.menu}
+              checked={this.state.activeOption === types.menu}
+              changeFn={() => this.handleRadioButtonCHange(types.menu)}
+            >
+              Menu
+            </FormRadio>
+            <FormRadio
+              id={types.news}
+              checked={this.state.activeOption === types.news}
+              changeFn={() => this.handleRadioButtonCHange(types.news)}
+            >
+              News
+            </FormRadio>
+            <FormRadio
+              id={types.gallery}
+              checked={this.state.activeOption === types.gallery}
+              changeFn={() => this.handleRadioButtonCHange(types.gallery)}
+            >
+              Gallery
+            </FormRadio>
+          </div>
           <Input name="name" label="Name" maxLength={30} />
           <Input name="price" label="Price" />
           <Input name="image" label="Image" />
